107c2f8411bf56a1589662f9ff0a96b65ec7ea1e
unknown <varadesharad@gmail.com>
2020-04-26 11:46:16 +0530

    ADDED[usecase2]:To add Last Name

--- User-Registration-Form.sh
+++ User-Registration-Form.sh
@@ -11,3 +11,18 @@ else
 		echo invalid
 fi
 
+
+echo " "
+echo " "
+
+
+echo "Enter the user Last Name"
+read user
+pat='^[A-Z]{1}[a-z]{3,}$'
+if [[ $user =~ $pat ]];
+then
+		echo valid
+else
+		echo invalid
+fi
+
